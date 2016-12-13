46ff8f9466582109e5a2176c277a811662334095
wangke168 <wangke168@gmail.com>
2016-12-13 11:49:02 +0800

    test

--- app/Http/Controllers/Control/MenuController.php
+++ app/Http/Controllers/Control/MenuController.php
@@ -10,15 +10,15 @@ use EasyWeChat\Foundation\Application;
 
 class MenuController extends Controller
 {
-/*    public $app;
+    public $app;
     public function __construct(Application $app)
     {
         $this->app=$app;
-    }*/
+    }
 
     public function menuList()
     {
-        return "221";
+    //    return "221";
 //        $ACCESS_TOKEN=$this->app->access_token->getToken();
         $token_url = "http://wechat.hengdianworld.com/hd-token";
         $ACCESS_TOKEN = file_get_contents($token_url);
