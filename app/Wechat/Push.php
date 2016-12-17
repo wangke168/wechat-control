c2e6f0c546db71d24c7183f42a52e0de3d9fa33b
wangke168 <wangke168@gmail.com>
2016-12-17 10:20:52 +0800

    演艺秀推送

--- app/Wechat/Push.php
+++ app/Wechat/Push.php
@@ -18,17 +18,12 @@ class Push
                 ->get();
             foreach ($rows as $row) {
                 echo "<optgroup label=\"" . $row->zone_name . "\">";
-
                 $result_options = DB::table('tour_project_class')
                     ->whereRaw('id not in (select project_id from tour_project_info)')
                     ->where('zone_classid', $row->id)
                     ->get();
-//                    $result_option = mysql_query("select * from wx_menu_list where online='1' and parents_id='" . $row["ID"] . "' order by id asc");
-
                 foreach ($result_options as $result_option) {
-
                     echo "<option value=\"" . $result_option->id . "\">" . $result_option->project_name . "</option>";
-
                 }
 
                 echo "</optgroup>";
@@ -43,19 +38,13 @@ class Push
                 $result_options = DB::table('tour_project_info')
                     ->where('zone_id', $row->id)
                     ->get();
-//                    $result_option = mysql_query("select * from wx_menu_list where online='1' and parents_id='" . $row["ID"] . "' order by id asc");
-
                 foreach ($result_options as $result_option) {
-
-//                    echo "<option value=\"" . $result_option->id . "\">" . $result_option->project_name . "</option>";
-
                     if ($id == $result_option->id) {
                         echo "<option selected  value=\"" . $result_option->id . "\">" . $result_option->show_name . "</option>";
                     } else {
                         echo "<option  value=\"" . $result_option->id . "\">" . $result_option->show_name . "</option>";
                     }
                 }
-
                 echo "</optgroup>";
             }
         }
