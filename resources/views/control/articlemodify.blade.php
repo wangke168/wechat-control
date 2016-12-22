f861cd9d49d441fa81504124935291f21689c93c
wangke168 <wangke168@gmail.com>
2016-12-22 13:15:03 +0800

    编辑器

--- resources/views/control/articlemodify.blade.php
+++ resources/views/control/articlemodify.blade.php
@@ -110,9 +110,9 @@
                                         if ($row->picurl) {
                                             if (strstr($row->picurl, '/editor/attached/image/')) {
                                                 $picurl = str_replace("/control/editor/attached/image/", "http://weix2.hengdianworld.com/control/editor/attached/image/", $row->picurl);
-                                              //  echo '<img src=' . $picurl . '>';
+                                                echo '<img src=' . $picurl . '>';
                                             } else {
-                                           //     echo '<img src=/' . $row->picurl . '>';
+                                                echo '<img src=/' . $row->picurl . '>';
                                             }
                                         }
                                         ?>
