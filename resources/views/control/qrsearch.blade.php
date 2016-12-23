a4471bfce7bb8a2b97666f13b77485d9c2316f88
wangke168 <wangke168@gmail.com>
2016-12-23 17:08:14 +0800

    二维码搜索

--- resources/views/control/qrsearch.blade.php
+++ resources/views/control/qrsearch.blade.php
@@ -4,6 +4,10 @@
 @section('title', '横店影视城微信管理平台－－－二维码管理')
 
 @section('page-title','二维码管理')
+@section('css')
+    <link href="{{asset('assets/global/plugins/bootstrap-modal/css/bootstrap-modal-bs3patch.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/global/plugins/bootstrap-modal/css/bootstrap-modal.css')}}" rel="stylesheet" type="text/css"/>
+@stop
 @section('page-menu-title')
     二维码搜索
 @stop
@@ -92,9 +96,9 @@
                                     </td>
                                     <td>
                                         <?php
-                                        echo "<a href='http://weix2.hengdianworld.com/control/qr_create.php?qrscene_id=" . $row->qrscene_id . "' class='label label-warning' target='_blank'><i class=\"icon-edit\"></i>获取二维码</a>&nbsp;&nbsp;&nbsp;";
+                                        echo "<a class='getqrcode label label-primary' data-target=\"#long\" data-toggle=\"modal\" data-src=\"qrcode_create/" . $row->qrscene_id . "\"><i class=\"fa  fa-download\"></i>&nbsp;下载二维码</a>&nbsp;";
 
-                                        echo "<a href='qrmodify?action=modify&id=" . $row->id . "' class='label label-success'><i class=\"icon-edit\"></i>修改</a>&nbsp;&nbsp;&nbsp;";
+                                        echo "<a href='qrmodify?action=modify&id=" . $row->id . "' class='label label-success'><i class=\"fa fa-edit\"></i>&nbsp;修改</a>";
 
                                         ?>
                                     </td>
@@ -102,7 +106,21 @@
                             @endforeach
 							</tbody>
 							</table>
-
+                            <!--弹出层-->
+                            <div id="long" class="modal fade " tabindex="-1" data-replace="true">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                                    <h4 class="modal-title">二维码下载</h4>
+                                </div>
+                                <div class="modal-body">
+                                    {{--<img id='qr'  src="../../../../../../i.imgur.com/KwPYo.jpg">--}}
+                                    <iframe id='qr' src="http://www.baidu.com" style="border:none; width:500px; height:500px;"></iframe>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" data-dismiss="modal" class="btn btn-default">关闭</button>
+                                </div>
+                            </div>
+                            <!--结束弹出层-->
 						</div>
 					</div>
 					<!-- END EXAMPLE TABLE PORTLET-->
@@ -110,5 +128,16 @@
 			</div>
 
 @stop
+@section('js')
+    <script src="{{asset('assets/global/plugins/bootstrap-modal/js/bootstrap-modalmanager.js')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/global/plugins/bootstrap-modal/js/bootstrap-modal.js')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/admin/pages/scripts/ui-extended-modals.js')}}"></script>
+@stop
+@section('init')
 
+    $(".getqrcode").click(function () {
+    //     alert($(this).attr('data-src'));
+    $("#qr").attr({"src": $(this).attr("data-src")});
+    });
 
+@stop
