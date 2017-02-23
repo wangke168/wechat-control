0c9301cdb7dd25eeb8c8765af11ae7b8686c9254
wangke168 <wangke168@gmail.com>
2017-02-23 09:57:59 +0800

    test

--- app/Http/Controllers/Control/RequestController.php
+++ app/Http/Controllers/Control/RequestController.php
@@ -65,13 +65,31 @@ class RequestController extends Controller
                 break;
             case 'save':
 
-                $filename=$request->file('file')->getClientOriginalName();
+                $filename=$request->file('file');
+                $file = $filename;
+                //判断文件上传过程中是否出错
+                if (!$file->isValid()) {
+                    exit('文件上传出错！');
+                }
+                $destPath = 'uploads/' . date('Ymd') . '/';
+                if (!file_exists($destPath))
+                    mkdir($destPath, 0755, true);
+                $extension = $file->getClientOriginalExtension();
+                $filename = time() . str_random(5) . '.' . $extension;
+
+            $filename = time() . str_random(5) . $file->getClientOriginalName();
+                if (!$file->move($destPath, $filename)) {
+                    exit('保存文件失败！');
+                }
+                $file= public_path().'/'.$destPath . $filename;
+//                print_r($_FILES);
+
 //                dd($request->file());
-                $path=$request->file->path();
-                $file = Input::file('photo');
-                $filename=$path.'/'.$filename;
-                return $request->file('file')->getPathInfo();
-                $result = $this->material->uploadImage($filename);  // 请使用绝对路径写法！除非你正确的理解了相对路径（好多人是没理解对的）！
+//                $path=$request->file->path();
+//                $file = Input::file('photo');
+//                $filename=$request->file('file')->getPathname();
+//                return $request->file('file')->getPathInfo();
+                $result = $this->material->uploadVoice($file);  // 请使用绝对路径写法！除非你正确的理解了相对路径（好多人是没理解对的）！
                 var_dump($result);
                 break;
             default:
