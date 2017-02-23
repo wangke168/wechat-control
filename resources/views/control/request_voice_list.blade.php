2ef0c3b459b40c4eab50ab2c78037d8ef62124b7
wangke168 <wangke168@gmail.com>
2017-02-23 15:32:07 +0800

    test

--- resources/views/control/request_voice_list.blade.php
+++ resources/views/control/request_voice_list.blade.php
@@ -119,13 +119,14 @@
                                     </td>
                                     <td>
                                         <?php
-                                    //    echo "<a href='requestvoice?action=download&id=" . $row->id . "' class='label label-success'><i class=\"fa fa-edit\"></i>&nbsp;下载</a>&nbsp;";
                                        if ($row->online == 1) {
                                             echo "<a OnClick=\"javascript:if (!confirm('是否真的要下线'))return false;\"  href='requestvoice?action=offline&id=" . $row->id . "' class='label label-warning'><i class=\"fa  fa-arrow-circle-o-down\"></i>&nbsp;下线</a>&nbsp;";
                                         } elseif ($row->online == 0) {
                                             echo "<a OnClick=\"javascript:if (!confirm('是否真的要上线'))return false;\"  href='requestvoice?action=online&id=" . $row->id . "' class='label label-success'><i class=\"fa  fa-arrow-circle-o-up\"></i>&nbsp;上线</a>&nbsp;";
 
                                         }
+                                        echo "<a class='getqrcode label label-primary' data-target=\"#long\" data-toggle=\"modal\" data-src=\"qrcode_create/" . $row->eventkey . "\"><i class=\"fa  fa-edit\"></i>&nbsp;试听</a>&nbsp;";
+
                                         echo "<a OnClick=\"javascript:if (!confirm('是否真的要删除'))return false;\"  href='requestvoice?action=del&id=" . $row->id . "&media_id=".$row->media_id."' class='label label-danger'><i class='fa fa-trash-o'></i>&nbsp;删除</a>";
                                         ?>
                                     </td>
@@ -136,7 +137,21 @@
                         </table>
                         {!! $rows->render() !!}
 
-
+                                <!--弹出层-->
+                        <div id="long" class="modal fade " tabindex="-1" data-replace="true">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                                <h4 class="modal-title">扫描二维码关注试听</h4>
+                            </div>
+                            <div class="modal-body">
+                                {{--<img id='qr'  src="../../../../../../i.imgur.com/KwPYo.jpg">--}}
+                                <iframe id='qr' src="http://www.baidu.com" style="border:none; width:500px; height:500px;"></iframe>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" data-dismiss="modal" class="btn btn-default">关闭</button>
+                            </div>
+                        </div>
+                        <!--结束弹出层-->
 
 
                     </div>
@@ -159,6 +174,9 @@
 
 @section('init')
 
+    $(".getqrcode").click(function () {
+    //     alert($(this).attr('data-src'));
+    $("#qr").attr({"src": $(this).attr("data-src")});
+    });
 
-    @stop
-
+@stop
