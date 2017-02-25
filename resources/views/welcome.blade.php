1e56dc838d9ffdd029af085a38eae3fc13d32560
wangke168 <wangke168@gmail.com>
2017-02-25 12:04:51 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Welcome WANG</div>
+                <div class="title">Welcome</div>
             </div>
         </div>
     </body>
