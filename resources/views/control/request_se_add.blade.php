7de128210a57e873b93de00acc5304c13bf0a963
wangke168 <wangke168@gmail.com>
2017-03-28 10:15:33 +0800

    test

--- resources/views/control/request_se_add.blade.php
+++ resources/views/control/request_se_add.blade.php
@@ -305,7 +305,9 @@
 
 @section('javascript')
     <script type="text/javascript">
-        var ue = UE.getEditor('container');
+        var ue = UE.getEditor('container',{
+            catchRemoteImageEnable: true
+        });
         ue.ready(function () {
             ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
         });
