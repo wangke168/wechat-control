6637d03290984f31864595f1605dc0c7eaa97f82
wangke168 <wangke168@gmail.com>
2017-07-05 15:34:30 +0800

    test

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -41,9 +41,21 @@ class TestController extends Controller
 
     public function test()
     {
-        $api=new Api();
+        $token_url = "https://wechat.hdymxy.com/hd-token";
+//        $ACCESS_TOKEN = file_get_contents($token_url);
+//        return $ACCESS_TOKEN;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$token_url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+
+        /*$api=new Api();
         $result= $api->get_wechat_api();
-        return $result['token'];
+        return $result['token'];*/
 
 
 
