fcdbc3ee500183b873ea04f2d7ea24e778de3eac
wangke168 <wangke168@gmail.com>
2017-07-05 15:58:13 +0800

    test

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -48,6 +48,7 @@ class TestController extends Controller
         curl_setopt($ch, CURLOPT_URL,$token_url);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         $result = curl_exec($ch);
         curl_close($ch);
