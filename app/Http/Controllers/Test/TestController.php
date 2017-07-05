0f314f90311e457795f4006ef16023f27151a393
wangke168 <wangke168@gmail.com>
2017-07-05 17:06:22 +0800

    test

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -45,20 +45,11 @@ class TestController extends Controller
        /* $ACCESS_TOKEN = file_get_contents($url);
         return $ACCESS_TOKEN;*/
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_REFERER, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        return $result;
+
 
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$token_url);
+        curl_setopt($ch, CURLOPT_URL,$url);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 
