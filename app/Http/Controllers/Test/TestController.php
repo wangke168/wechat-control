b2c11ad12e48da2a2bf3b022d8a407d6f2b1beff
wangke168 <wangke168@gmail.com>
2017-07-05 17:11:41 +0800

    test

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -49,16 +49,15 @@ class TestController extends Controller
 
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_REFERER, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         $result = curl_exec($ch);
-        var_dump(curl_error($ch));
         curl_close($ch);
-        var_dump( $result);
-
+        return $result;
 
 
     }
