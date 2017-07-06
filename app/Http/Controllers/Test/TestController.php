b35336963d964c2c1a367dc21e045958f8b11128
wangke168 <wangke168@gmail.com>
2017-07-06 08:35:58 +0800

    test

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -41,9 +41,24 @@ class TestController extends Controller
 
     public function test()
     {
-        $token_url = "http://localhost:8080/api";
-        $result = file_get_contents($token_url);
-        $result = json_decode($result, true);
+        $url = "http://localhost:8080/api";
+       /* $ACCESS_TOKEN = file_get_contents($url);
+        return $ACCESS_TOKEN;*/
+
+
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT,60);  //只需要设置一个秒的数量就可以
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_REFERER, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $result = curl_exec($ch);
+        var_dump(curl_error($ch));
+        curl_close($ch);
         return $result;
 
 
