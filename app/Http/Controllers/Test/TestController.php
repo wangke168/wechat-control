23872d6157f5c59442367eb287ae9a075270b4fc
wangke168 <wangke168@gmail.com>
2017-07-06 08:34:00 +0800

    test

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -41,45 +41,14 @@ class TestController extends Controller
 
     public function test()
     {
-        $url = "http://localhost:8080/hd-token";
-       /* $ACCESS_TOKEN = file_get_contents($url);
-        return $ACCESS_TOKEN;*/
-
-
-
-        $token_url = "https://wechat.hdymxy.com/api";
-//        $ACCESS_TOKEN = file_get_contents($token_url);
-//        return $ACCESS_TOKEN;
-        $result =  $this->getSslPage($token_url);
+        $token_url = "http://localhost:8080/api";
+        $result = file_get_contents($token_url);
         $result = json_decode($result, true);
         return $result;
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_HEADER, false);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_TIMEOUT,60);  //只需要设置一个秒的数量就可以
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_REFERER, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        $result = curl_exec($ch);
-        var_dump(curl_error($ch));
-        curl_close($ch);
-        return $result;
-
 
     }
-    private function getSslPage($url) {
-        /*  http://www.manongjc.com/article/1428.html */
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        $result = curl_exec($ch);
-        curl_close($ch);
-        return $result;
-    }
+
 
     private function decode_mime($string)
     {
