57edb2e8698d2686f82c1de72f0773d84a79c6e7
wangke168 <wangke168@gmail.com>
2017-11-01 11:59:43 +0800

    test

--- app/Http/Controllers/Control/AgentController.php
+++ app/Http/Controllers/Control/AgentController.php
@@ -16,6 +16,7 @@ class AgentController extends Controller
 
     public function __construct()
     {
+        phpinfo();
 //        $this->SoapClint = new SoapClient("http://aaa.hdyuanmingxinyuan.com/interface/AgentInterface.asmx?WSDL");
         $this->SoapClint = new SoapClient("http:///10.0.61.201/interface/AgentInterface.asmx?WSDL");
 //        $this->CompanyCode='ymxytest0fjloa';
