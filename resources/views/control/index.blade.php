c83a1124bb6636c77bfa1f499d88f197c8d6c292
wangke168 <wangke168@gmail.com>
2017-11-02 16:14:28 +0800

    test

--- resources/views/control/index.blade.php
+++ resources/views/control/index.blade.php
@@ -131,6 +131,82 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="dashboard-stat yellow-casablanca">
+                    <div class="visual">
+                        <i class="fa fa-comments"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                                同步代理商订单
+
+                        </div>
+                        {{--<div class="desc">
+
+                        </div>--}}
+                    </div>
+                    <a class="more" href="/control/agentinterface">
+                        View more <i class="m-icon-swapright m-icon-white"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="dashboard-stat grey-cascade">
+                    <div class="visual">
+                        <i class="fa fa-bar-chart-o"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+                            取消代理商订单
+                        </div>
+                       {{-- <div class="desc">
+                            昨日取消关注数
+                        </div>--}}
+                    </div>
+                    <a class="more" href="/control/agentordercancel">
+                        View more <i class="m-icon-swapright m-icon-white"></i>
+                    </a>
+                </div>
+            </div>
+           {{-- <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="dashboard-stat green-haze">
+                    <div class="visual">
+                        <i class="fa fa-shopping-cart"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                        </div>
+                        <div class="desc">
+                            同步订单
+                        </div>
+                    </div>
+                    <a class="more" href="ordercount">
+                        View more <i class="m-icon-swapright m-icon-white"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                <div class="dashboard-stat purple-plum">
+                    <div class="visual">
+                        <i class="fa fa-globe"></i>
+                    </div>
+                    <div class="details">
+                        <div class="number">
+
+                        </div>
+                        <div class="desc">
+                            昨日总阅读数
+                        </div>
+                    </div>
+                    <a class="more" href="#">
+                        View more <i class="m-icon-swapright m-icon-white"></i>
+                    </a>
+                </div>
+            </div>--}}
+        </div>
         <!-- END DASHBOARD STATS -->
         <div class="clearfix">
         </div>
