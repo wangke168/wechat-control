b319833ba0c28d83a941f67c9e9bc8b28b9ca27d
wangke168 <wangke168@gmail.com>
2022-05-02 13:49:18 +0800

    添加图片素材

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Welcome WANG</div>
+                <div class="title">Hello World</div>
             </div>
         </div>
     </body>
